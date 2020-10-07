eec6d16619879c1ec655e9a23fd8dbb33912ba67
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-10-08 00:14:31 +0530

    Updates

---
+++ precache-manifest.041258c2f37fc7950daf24fe5f23cf08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a7eba42b7e13b6e8e051e2e7daa5e5",
+    "url": "/Covid-19_Tracker/index.html"
+  },
+  {
+    "revision": "626dbb4e258f0727a80f",
+    "url": "/Covid-19_Tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "7d75e2964b329567d9ca",
+    "url": "/Covid-19_Tracker/static/css/main.c70dcd59.chunk.css"
+  },
+  {
+    "revision": "626dbb4e258f0727a80f",
+    "url": "/Covid-19_Tracker/static/js/2.c8d8256d.chunk.js"
+  },
+  {
+    "revision": "6788bbbeb96ac5b9fb2d2231815fe398",
+    "url": "/Covid-19_Tracker/static/js/2.c8d8256d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d75e2964b329567d9ca",
+    "url": "/Covid-19_Tracker/static/js/main.0dc7ec80.chunk.js"
+  },
+  {
+    "revision": "40c88b699a208712f5cf",
+    "url": "/Covid-19_Tracker/static/js/runtime-main.e9cdc8b5.js"
+  }
+]);
